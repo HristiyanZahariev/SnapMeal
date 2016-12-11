fd8e7c1f3b2910c7c7cdaf344bdd7df9777869e2
HristiyanZahariev <ihzahariev@gmail.com>
2016-12-11 02:22:38 +0200

    Tried to add spring data but unsuccessful

--- src/main/java/com/snapmeal/Application.java
+++ src/main/java/com/snapmeal/Application.java
@@ -2,9 +2,13 @@ package com.snapmeal;
 
 import com.snapmeal.configuration.SpringConfiguration;
 import com.snapmeal.controllers.HelloController;
+import com.snapmeal.dao.Recipe;
 import com.snapmeal.services.HelloService;
+import com.snapmeal.services.RecipeService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 /**
  * Created by hristiyan on 08.12.16.
@@ -14,6 +18,13 @@ public class Application {
         ApplicationContext appContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
         HelloService helloService = (HelloService) appContext.getBean("helloService");
         HelloController helloController = (HelloController) appContext.getBean("helloController");
-        System.out.println(helloService.hello());
+       // PersistenceConfiguration persistenceConfiguration = (PersistenceConfiguration) appContext.getBean("persistenceConfiguration");
+//        RecipeService recipeService = appContext.getBean(RecipeService.class);
+//        Recipe recipe = new Recipe("Pesho", "sadsadsadasdas");
+//        recipeService.save(recipe);
+//        List<Recipe> recipes = recipeService.findAll();
+//        for (Recipe r : recipes) {
+//            System.out.println(r);
+//        }
     }
 }
