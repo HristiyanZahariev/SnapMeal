4bddec7fe67b4fcabcabc3e354ac436140dba1cf
HristiyanZahariev <ihzahariev@gmail.com>
2017-02-02 04:17:08 +0200

    Changed frontend design little bit

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import { AuthHttp, AuthConfig, AUTH_PROVIDERS, provideAuth } from 'angular2-jwt';
+import { CollapseDirective } from 'ng2-bootstrap'
 
 import { AppComponent }  from './app.component';
 import { UserComponent }  from './components/user.component';
